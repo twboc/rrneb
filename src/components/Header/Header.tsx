2f73b9830ac9e31fb5b5726e46e4373be4d64e16
twobc <hubert.sobkow@gmail.com>
2020-09-30 15:53:57 +0200

    Clean project and build external css files

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-
 import HeaderBase from './Header.base'
+import style from './style/Header.scss'
 
 class Header extends HeaderBase { 
 
     render() {
-        return <h2 onClick={this.handleClick}>Header Toggle: {this.state.toggle.toString()}</h2>;
+        return <div className={style.header} onClick={this.handleClick}>Header Toggle: {this.state.toggle.toString()}</div>;
     }
 }
 
