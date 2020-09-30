9b10d1558c6a76f64e542687928931a5e59f48e9
twobc <hubert.sobkow@gmail.com>
2020-09-30 16:38:30 +0200

    Styling native app and component structure

---
+++ src/components/Main/style/Main.native.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+const style = StyleSheet.create({
+    main: {
+      flex: 1,
+      padding: 0,
+      margin: 0,
+    },
+});
+
+export default style
